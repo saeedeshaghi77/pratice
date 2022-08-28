7913ad591fa2d417e1ade5f4984a9de2db854c4b
saeedeshaghi77 <saeedeshaghi77@gmail.com>
2022-08-28 17:51:27 +0430

    second

---
+++ j1.a1.q2.py
@@ -0,0 +1,8 @@
+i=0
+total=0
+while(i<10):
+    number= float(input(" Enter your number : "))
+    total= total+ number
+    i=i+1
+    print('total number',i,':',total )
+print( 'average : ', total/10)    
